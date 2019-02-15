5892a7eebca1d8255d6e817f3f6dc3da19203ca6
cmsrobostangs <43422377+cmsrobostangs@users.noreply.github.com>
2019-02-14 21:47:20 -0500

    Added PROS implementation

---
+++ PROS/src/opcontrol.cpp
@@ -0,0 +1,47 @@
+#include "okapi/api.hpp"
+using namespace okapi;
+
+// Chassis Controller - lets us drive the robot around with open- or closed-loop control
+auto drive = ChassisControllerFactory::create(
+  {20, 13}, {-19, -12},
+  AbstractMotor::gearset::green,
+  {4_in, 10.5_in}
+);
+
+void opcontrol() {
+  // Joystick to read analog values for tank or arcade control
+  // Master controller by default
+  Controller controller;
+
+  // Arm related objects
+  ControllerButton capFlipperUpButton(ControllerDigital::A);
+  ControllerButton capFlipperDownButton(ControllerDigital::B);
+	ControllerButton intakeInButton(ControllerDigital::R1);
+	ControllerButton intakeOutButton(ControllerDigital::R2);
+
+	Motor intakeMotor = 18_rmtr;
+  Motor capFlipperMotor = 8_rmtr;
+
+  while (true) {
+    // Tank drive with left and right sticks
+    drive.tank(controller.getAnalog(ControllerAnalog::leftY),controller.getAnalog(ControllerAnalog::rightY));
+
+		if (capFlipperUpButton.isPressed()) {
+			intakeMotor.move_voltage(-127);
+		} else if (capFlipperDownButton.isPressed()) {
+			intakeMotor.move_voltage(127);
+		} else {
+			intakeMotor.move_voltage(0);
+		}
+
+		if (intakeInButton.isPressed()) {
+			intakeMotor.move_voltage(-127);
+		} else if (intakeOutButton.isPressed()) {
+			intakeMotor.move_voltage(127);
+		} else {
+			intakeMotor.move_voltage(0);
+		}
+
+    pros::Task::delay(10);
+  }
+}
