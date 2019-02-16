5c5cff8b8bc518ab974bdd1d8f2ddf8d294d33cd
Robo <Robo@CLW-MROBO-10.os.oaklandschools.net>
2019-02-16 12:12:29 -0500

    backprogramming changes

--- BackProgramming/src/autonomous.cpp
+++ BackProgramming/src/autonomous.cpp
@@ -25,17 +25,20 @@ Motor intakeMotor = 18_rmtr;
    intakeMotor.move(-127);
    drive.moveDistance(48_in); // movng forward 39
    pros::Task::delay(100);
-   drive.moveDistance(-10_in);
+   drive.moveDistance(-16_in);
    drive.turnAngle(-90_deg); // turn left 90 degrees or at an right angle
-   //intakeMotor.move(-127); // reverse intake
+   pros::Task::delay(100);
    drive.moveDistance(16_in); // moving forward 16 inches
-   drive.turnAngle(-90);// turn at a left angle
    pros::Task::delay(100);
-   drive.turnAngle(90_deg); // turn around
+   drive.turnAngle(90);// turn at a left angle
    pros::Task::delay(100);
-   drive.moveDistance(10_in); // moving forward the sqare root of 1600 inches
+   drive.moveDistance(16_in); // moving forward the sqare root of 1600 inches
+   pros::Task::delay(100);
    drive.moveDistance(-12);
+   pros::Task::delay(100);
    drive.turnAngle(90_deg); // turn right
+   pros::Task::delay(100);
+   intakeMotor.move(0);
    drive.moveDistance(45_in);
    // pros::Task::delay(300);
 
